e722e719e7fa1efa8bfb48b5bcda24556752cebb
Abdessadek Hmiddouch <abdessadekhm@gmail.com>
2024-07-01 19:52:56 +0100

    Add File & Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./_root/pages"
+import SigninForm from "./_auth/forms/SigninForm"
+import SignupForm from "./_auth/forms/SignupForm"
 import "./globals.css"
+import AuthLayout from "./_auth/AuthLayout"
+import RootLayout from "./_root/RootLayout"
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+        {/*public routes*/}
+        <Route element={<AuthLayout/>}>
+            <Route path="/sign-in" element = {<SigninForm/>} />
+            <Route path="/sign-up" element = {<SignupForm/>} />
+        </Route>
+        <Route element={<RootLayout/>}>
+            <Route index element = {<Home/>}/>
+        </Route>
+        {/*private routes*/}
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import  {createRoot}  from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);
 
 root.render(
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 )
